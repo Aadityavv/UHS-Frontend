1085ba8d265a601dc6f902e08095d8ba73993f1a
Aadityavv <aadityavv9@gmail.com>
2025-02-17 19:29:08 +0530

    changes added

--- src/private/user-dashboard/UserDashboard.tsx
+++ src/private/user-dashboard/UserDashboard.tsx
@@ -78,7 +78,7 @@ const UserDashboard = () => {
       <Toaster />
       
       {/* Main Content */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 ">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
