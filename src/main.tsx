3ea23586346380539068dfc401e11f567eba846b
Aadityavv <aadityavv9@gmail.com>
2025-03-17 18:59:55 +0530

    new updated

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router-dom.tsx";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
