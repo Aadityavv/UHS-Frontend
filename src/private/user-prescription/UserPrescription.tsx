55403d6ae9cb4a212f65fbdeae14867ff5230c37
Aadityavv <aadityavv9@gmail.com>
2025-03-18 17:57:50 +0530

    new updates added

--- src/private/user-prescription/UserPrescription.tsx
+++ src/private/user-prescription/UserPrescription.tsx
@@ -239,7 +239,7 @@ const UserPrescription = () => {
                     )}
                   >
                     <CalendarIcon className="mr-2 h-4 w-4" />
-                    {date ? format(date, "PPP") : <span>Pick a date</span>}
+                    {date ? format(date, "PPPP") : <span>Pick a date</span>}
                   </Button>
                 </PopoverTrigger>
                 <PopoverContent className="w-auto p-0">
