0787c61083681115b16958a68315c5c41c71b87b
Aadityavv <aadityavv9@gmail.com>
2025-03-23 15:46:20 +0530

    Added package for insights

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,9 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/router-dom.tsx";
 import "tippy.js/dist/tippy.css";
 import "tippy.js/animations/scale.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
       <RouterProvider router={router} />
-      <SpeedInsights></SpeedInsights>
   </StrictMode>
 );
