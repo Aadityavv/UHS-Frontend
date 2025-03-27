5a7492f70527cf58d29ffb10ee5014ba41f8c131
Aadityavv <aadityavv9@gmail.com>
2025-03-27 12:38:06 +0530

    Security Improved

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,24 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:true
+  server: {
+    host: true
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    },
+    minify: 'esbuild', // default, ensures JS is minified
+    sourcemap: false   // prevent exposing source maps
   }
 });
